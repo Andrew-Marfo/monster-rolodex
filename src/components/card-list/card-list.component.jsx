1805f7340ea70526bb9c2f238d656632f23a7f72
Andrew Marfo <andrewmarfo23@gmail.com>
2024-09-09 00:33:19 +0000

    card list component

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class CardList extends Component{
+
+    render() {
+        return (
+            <div>
+                Hi there I am the card list Component
+             </div>
+        )
+    }
+}
