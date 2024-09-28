ee67f3185b9209b6587c0c20d77d91b0de6f3e1c
Andrew Marfo <andrewmarfo23@gmail.com>
2024-09-28 11:25:35 +0000

    converted card list component to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,21 +1,11 @@
-import { Component } from "react";
-
 import './card-list.styles.css'
 import MonsterCard from "../monster-card/monster-card.component";
 
-class CardList extends Component {
-
-    render() {
-        const { monsters } = this.props;
-        return (
-            <div className="card-list">
-                {
-                    monsters.map((monster) => {
-                        return <MonsterCard monster={monster} />
-                    })
-                }
-            </div>
-        )
-    }
-}
+const CardList = ({ monsters }) => (
+    <div className="card-list">
+        {monsters.map((monster) => {
+            return <MonsterCard monster={monster} />
+        })}
+    </div>
+)
 export default CardList;
